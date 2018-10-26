63aa657fcf27b311041cd73adf9d7987e5cc0aba
quinroos <quinroos@student.arteveldehs.be>
2018-10-26 11:22:39 +0200

    made sum and multiply function

--- ex02_calculator/script.js
+++ ex02_calculator/script.js
@@ -1,21 +1,35 @@
-const sumSubmitBtn;
-const sumResultContainer;
-const productSubmitBtn;
-const productResultContainer;
-const numberInputContainer;
-const addInputBtn;
-const numberCountContainer;
+const sumSubmitBtn = document.querySelector('#sum-submit-btn');
+const sumResultContainer = document.querySelector('#sum-result-container');
+const productSubmitBtn = document.querySelector('#product-submit-btn');
+const productResultContainer = document.querySelector('#product-result-btn');
+const numberInputContainer = document.querySelector('#input-container');
+const addInputBtn = document.querySelector('#add-input-btn');
+const numberCountContainer = document.querySelector('#number-count-container');
+let number = parseInt(document.querySelectorAll('.number-input').value);
 
 function sumNumbers() {
-  // return the sum value of all the numbers
+    // return the sum value of all the numbers
+    const numberInputs = document.querySelectorAll('.number-input').value;
+    let totalSum = 0;
+    for (let i = 0; i < numberInputs.length; i++){
+        let numberInput = numberInputs[i];
+        totalSum += parseInt(numberInput.value);
+    }
+    return totalSum;
 }
-
 function printSum() {
-  // print the sum value of all the numbers
+    // print the sum value of all the numbers
 }
 
 function multiplyNumbers(){
   // return the product of all the numbers
+  const numberInputs = document.querySelectorAll('.number-input').value;
+    let totalProduct = 0;
+    for (let i = 0; i < numberInputs.length; i++){
+        let numberInput = numberInputs[i];
+        totalProduct *= parseInt(numberInput.value);
+    }
+    return totalProduct;
 }
 
 function printProduct() {
@@ -35,3 +49,6 @@ function removeNumberInput(event) {
 }
 
 // add event listeners
+sumSubmitBtn.addEventListener('click', printSum);
+productSubmitBtn.addEventListener('click', printProduct);
+addInputBtn.addEventListener('click', addNumberInput);

61ae9a849d70700e9a5c51a2cb5a38990114468a
DANNY EERAERTS <danny@MacBook-Pro-van-DANNY.local>
2018-10-26 11:26:02 +0200

    Sum and Product Function

--- ex02_calculator/script.js
+++ ex02_calculator/script.js
@@ -1,21 +1,37 @@
-const sumSubmitBtn;
-const sumResultContainer;
-const productSubmitBtn;
-const productResultContainer;
-const numberInputContainer;
-const addInputBtn;
-const numberCountContainer;
+const sumSubmitBtn = document.querySelector("#sum-submit-btn");
+const sumResultContainer = document.querySelector("#sum-result-container") ;
+const productSubmitBtn = document.querySelector("#product-submit-btn");
+const productResultContainer = document.querySelector("#product-result-container");
+const numberInputContainer = document.querySelector("#input-container");
+const addInputBtn =document.querySelector("#add-input-btn");
+const numberCountContainer = document.querySelector("#number-count-container");
 
 function sumNumbers() {
   // return the sum value of all the numbers
+  const numberInputs = document.querySelectorAll(".number-input")
+  let totalSum = 0;
+  for (let i=0; i < numberInputs.length;i++){
+    let numberInput = numberInputs[i]
+    totalSum += parseInt(numberInput.value);
+  }
+  return totalSum;
 }
 
 function printSum() {
   // print the sum value of all the numbers
+  
+
 }
 
 function multiplyNumbers(){
   // return the product of all the numbers
+  const numberInputs = document.querySelectorAll(".number-input")
+  let totalProduct = 0;
+  for (let i=0; i < numberInputs.length;i++){
+    let numberInput = numberInputs[i]
+    totalProduct *= parseInt(numberInput.value);
+  }
+  return totalProduct;
 }
 
 function printProduct() {
@@ -35,3 +51,15 @@ function removeNumberInput(event) {
 }
 
 // add event listeners
+let snumber_01 = document.querySelector ("#number-01").value;
+let snumber_02 = document.querySelector ("#number-02").value;
+let isom = parseInt(snumber_01) + parseInt(snumber_02);
+let iverschil = parseInt(snumber_01) - parseInt(snumber_02);
+let iproduct = parseInt(snumber_01) * parseInt(snumber_02);
+let ideling = parseInt(snumber_01) / parseInt(snumber_02);
+let fomzetting = parseFloat(ideling);
+let iresultaat = Math.round(fomzetting*100)/100;
+document.getElementById("result1").innerHTML = isom;
+document.getElementById("result2").innerHTML = iverschil;
+document.getElementById("result3").innerHTML = iproduct;
+document.getElementById("result4").innerHTML = iresultaat;

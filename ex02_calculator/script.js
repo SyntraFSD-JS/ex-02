058933b699cc7723c7f50866172459b02a2a2beb
Koen Michiels <koen.michiels@telenet.be>
2018-10-26 11:41:54 +0200

    oef2

--- ex02_calculator/script.js
+++ ex02_calculator/script.js
@@ -1,25 +1,45 @@
-const sumSubmitBtn;
-const sumResultContainer;
-const productSubmitBtn;
-const productResultContainer;
-const numberInputContainer;
-const addInputBtn;
-const numberCountContainer;
+const sumSubmitBtn = document.querySelector("#sum-submit-btn");
+const sumResultContainer = document.querySelector("#sum-result-container");
+const productSubmitBtn = document.querySelector("#product-submit-btn");
+const productResultContainer = document.querySelector("#product-result-container");
+const numberInputContainer = document.querySelector("#input-container");
+const addInputBtn = document.querySelector("#add-input-btn");
+const numberCountContainer = document.querySelector("#number-count-container");
 
 function sumNumbers() {
   // return the sum value of all the numbers
+  const numberInputs = document.querySelectorAll(".number-input");
+  let totalSum = 0;
+  for(let i = 0; i < numberInputs.length; i++){
+    let numberInput = numberInputs[i];
+    totalSum += parseInt(numberInput.value);
+    console.log(totalSum);
+    return totalSum;
+  }
 }
 
 function printSum() {
   // print the sum value of all the numbers
+  sumResultContainer.textContent = sumNumbers();
+
 }
 
 function multiplyNumbers(){
   // return the product of all the numbers
+  const numberInputs = document.querySelectorAll(".number-input");
+  let product = 1;
+  for(let i = 0; i < numberInputs.length; i++){
+    let numberInput = numberInputs[i];
+    product *= parseInt(numberInput.value);
+    return Product;
+  }
+
+
 }
 
 function printProduct() {
   // print the product of all the numbers
+  productResultContainer.textContent = multiplyNumbers();
 }
 
 function printNumberCount(){

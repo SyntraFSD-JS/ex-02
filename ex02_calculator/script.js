3ce9a81123f7d302ae1dcc28ab235f174b2c9060
simonpeeters1995 <simsimpeeeters@gmail.com>
2018-10-26 11:25:16 +0200

    commit

--- ex02_calculator/script.js
+++ ex02_calculator/script.js
@@ -1,10 +1,10 @@
-const sumSubmitBtn;
-const sumResultContainer;
-const productSubmitBtn;
-const productResultContainer;
-const numberInputContainer;
-const addInputBtn;
-const numberCountContainer;
+const sumSubmitBtn= document.querySelector("#sum-submit-btn");
+const sumResultContainer= document.querySelector("#sum-result-container");
+const productSubmitBtn=document.querySelector("#product-submit-btn");
+const productResultContainer=document.querySelector("#product-result-container");
+const numberInputContainer=document.querySelectorAll(".input number-input");
+const addInputBtn=document.querySelector("#add-input-btn");
+const numberCountContainer=document.querySelector("#number-count-container");
 
 function sumNumbers() {
   // return the sum value of all the numbers

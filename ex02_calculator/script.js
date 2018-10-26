7b2552b0dfc7ec77794cc738045a28493346f8d8
Vlad <vlad@Vlads-MacBook-Pro.local>
2018-10-26 12:03:03 +0200

    RemoveNumber

--- ex02_calculator/script.js
+++ ex02_calculator/script.js
@@ -1,21 +1,36 @@
-const sumSubmitBtn;
-const sumResultContainer;
-const productSubmitBtn;
-const productResultContainer;
-const numberInputContainer;
-const addInputBtn;
-const numberCountContainer;
+const sumSubmitBtn = document.querySelector('#sum-submit-btn');
+const sumResultContainer = document.querySelector('sum-resolt-container');
+const productSubmitBtn = document.querySelector('product-submit-btn');
+const productResultContainer = document.querySelector('product-resolt-container');
+const numberInputContainer = document.querySelector('input-container');
+const addInputBtn = document.querySelector('#add-input-btn');
+const numberCountContainer = document.querySelector('#number-count-container');
 
 function sumNumbers() {
   // return the sum value of all the numbers
+  const numberInputs = document.querySelectorAll("number-input");
+  let totalSum = 0;
+
+  for(let i = 0; i < numberInputs.length; i++) {
+      let numberInput = numberInputs[i]; 
+       totalSum += parseInt(numberInput.value);
+  }
+return totalSum;
 }
 
 function printSum() {
   // print the sum value of all the numbers
+  sumResultContainer.textContent = sumNumbers();
 }
 
 function multiplyNumbers(){
   // return the product of all the numbers
+  const addNumberInput = document.querySelectorAll("number-input");
+  let totalProduct = 1;
+
+  for(let i = 0; i < 1 lenght; i++) {
+      let numberInput = numberInput.length; [i]) {
+       totalProduct *= parseInt(numberInput.value);
 }
 
 function printProduct() {
@@ -32,6 +47,9 @@ function addNumberInput(){
 
 function removeNumberInput(event) {
   // remove a number input
+
 }
 
 // add event listeners
+sumSubmitBtn.addEventListener('click'), printSum);
+sumSubmitBtn.addEventListener('click'), printProduct);

bf6c5d11b5f7093d3d2af27702243e3e926364c4
Korneel Eeckhout <korneel@pienter.tech>
2018-11-21 10:28:35 +0100

    Split solution

--- ex02_calculator/script.js
+++ ex02_calculator/script.js
@@ -1,79 +1,37 @@
-const sumSubmitBtn = document.querySelector('#sum-submit-btn');
-const sumResultContainer = document.querySelector('#sum-result-container');
-const productSubmitBtn = document.querySelector('#product-submit-btn');
-const productResultContainer = document.querySelector('#product-result-container');
-const numberInputContainer = document.querySelector('#input-container');
-const addInputBtn = document.querySelector('#add-input-btn');
-const numberCountContainer = document.querySelector('#number-count-container');
+const sumSubmitBtn;
+const sumResultContainer;
+const productSubmitBtn;
+const productResultContainer;
+const numberInputContainer;
+const addInputBtn;
+const numberCountContainer;
 
 function sumNumbers() {
   // return the sum value of all the numbers
-  const numberInputs = document.querySelectorAll('.number-input');
-  let totalSum = 0;
-  numberInputs.forEach(function (numberInput) {
-    totalSum += parseInt(numberInput.value);
-  });
-  return totalSum;
 }
 
 function printSum() {
   // print the sum value of all the numbers
-  sumResultContainer.textContent = sumNumbers();
 }
 
-function multiplyNumbers(){
+function multiplyNumbers() {
   // return the product of all the numbers
-  const numberInputs = document.querySelectorAll(".number-input");
-  let totalProduct = 1;
-  for (let i = 0; i < numberInputs.length; i++) {
-    let numberInput = numberInputs[i];
-    totalProduct *= parseInt(numberInput.value);
-  }
-  return totalProduct;
 }
 
 function printProduct() {
   // print the product of all the numbers
-  productResultContainer.textContent = multiplyNumbers();
 }
 
-function realTime(){
-  printProduct();
-  printSum();
-}
-
-function printNumberCount(){
+function printNumberCount() {
   // get and print the number of input fields
-  const numberOfInputs = numberInputContainer.children.length;
-  // same result as >
-  // const numberOfInputs = document.querySelectorAll('.input-number').length;
-  numberCountContainer.textContent = numberOfInputs;
 }
 
-function addNumberInput(){
+function addNumberInput() {
   // add a number input
-  const existingInputGroup = document.querySelector('.number-input-group');
-  const newInputGroup = existingInputGroup.cloneNode(true);
-  const newNumberInput = newInputGroup.querySelector('.number-input');
-  newNumberInput.value = '0';
-  numberInputContainer.appendChild(newInputGroup);
-  printNumberCount();
-  realTime();
 }
 
-function removeNumberInput(eventInformation) {
+function removeNumberInput(event) {
   // remove a number input
-  if (eventInformation.target.matches('.delete-number-input') && numberInputContainer.children.length > 2) {
-    const selectedInputGroup = eventInformation.target.closest('.number-input-group');
-    selectedInputGroup.remove();
-    printNumberCount();
-    realTime();
-  }
 }
 
 // add event listeners
-sumSubmitBtn.addEventListener('click', printSum);
-productSubmitBtn.addEventListener('click', printProduct);
-addInputBtn.addEventListener('click', addNumberInput);
-numberInputContainer.addEventListener('click', removeNumberInput);
-numberInputContainer.addEventListener('input', realTime);
